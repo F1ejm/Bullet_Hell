0505dcba1418e05f14cb53171fc3c4751f3a4c9d
noone787 <witcherandmaster@op.pl>
2025-05-02 16:00:17 +0200

    stamina

--- UI/PlayerUI/stamina.gd
+++ UI/PlayerUI/stamina.gd
@@ -1,21 +1,27 @@
 extends Control
 
 @onready var progress_bar: ProgressBar = $ProgressBar
+@onready var timer: Timer = $Timer
+
+var dash:bool = false
 
 func _ready() -> void:
 	progress_bar.size.x = Global.Max_Stamina
 	progress_bar.max_value = Global.Max_Stamina
 	
-func _process(delta: float) -> void:
-	print(Global.Stamina)
+func _physics_process(delta: float) -> void:
+	
 	progress_bar.size.x = Global.Max_Stamina
 	progress_bar.max_value = Global.Max_Stamina
 	progress_bar.value = Global.Stamina
 	
 	#Regeneracja Staminy
-	if Global.Stamina < Global.Max_Stamina:
+	if Global.Stamina < Global.Max_Stamina and dash == false:
 		Global.Stamina += Global.Stamina_Regen * 80 * delta
-	
-	#Upewnienie Się Ze nie wychodzi poza limit
-	if Global.Stamina > Global.Max_Stamina:
-		Global.Stamina = Global.Max_Stamina
+
+	#Sprawdzenie Czy jest Dash
+	if dash == true:
+		timer.start()
+
+func _on_timer_timeout() -> void:
+	dash = false
