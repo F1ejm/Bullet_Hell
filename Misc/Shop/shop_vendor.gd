04bb0ec0773c2e556a0c6fc0bff9e49b2d5864be
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-05-02 16:27:51 +0200

    Revert "shop done"
    This reverts commit 0c1275227d698c878b77e5c2739244216c290d0e.

--- Misc/Shop/shop_vendor.gd
+++ Misc/Shop/shop_vendor.gd
@@ -1,6 +1,10 @@
 extends StaticBody2D
 
-var shop_open := false
+@onready var UI: Control = $CanvasLayer/UI
+
+func ready() -> void:
+	
+	UI.visible = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
@@ -10,21 +14,13 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		Global.shop_can_interact = false
 
-func _process(delta: float) -> void:
-	if Global.shop_can_interact == true:
-		Global.show_interaction = true
-	else:
-		Global.show_interaction = false
-
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("interact") and Global.shop_can_interact == true and shop_open == false:
-		shop_open = true
+	if event.is_action_pressed("interact") and Global.shop_can_interact == true:
 		Global.stop = true
 		print("open shop")
-		Global.shop_ui_visible = true
+		UI.visible = true
 
-	elif event.is_action_pressed("interact") and Global.shop_can_interact == true and shop_open == true:
-		shop_open = false
+	if event.is_action_pressed("back") and Global.shop_can_interact == true:
 		Global.stop = false
 		print("close shop")
-		Global.shop_ui_visible = false
+		UI.visible = false
