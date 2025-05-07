b52b229d088795d995d47aef7b1b18c54e34b8bb
F1ejm <karolosrodka@gmail.com>
2025-05-07 18:44:48 +0200

    generowanie cian + znikanie

--- Main/Obstacle/obstacle_template/template_controler.gd
+++ Main/Obstacle/obstacle_template/template_controler.gd
@@ -7,6 +7,7 @@ extends Node2D
 @export var template_2 : PackedScene
 @export var template_3 : PackedScene
 
+@export var Placeholder : Node2D
 
 var runda_play : bool
 var runda_num : int
@@ -19,6 +20,7 @@ func _ready() -> void:
 	runda_num = 0
 	runda_play = Global.IsRoundPlaying
 	runda_time = Global.Czas_Rundy
+	generate()
 
 func generate():
 	random = randi_range(0,2)
@@ -26,17 +28,17 @@ func generate():
 	match random : 
 		0: 
 			var b = template_1.instantiate()
-			add_child(b)
-			b.transform = midle.transform
+			Placeholder.add_child(b)
+			b.global_position = midle.global_position
 			
 		1: 
 			var b = template_2.instantiate()
-			add_child(b)
-			b.transform = midle.transform
+			Placeholder.add_child(b)
+			b.global_position = midle.global_position
 		2: 
-			var b = template_2.instantiate()
-			add_child(b)
-			b.transform = midle.transform
+			var b = template_3.instantiate()
+			Placeholder.add_child(b)
+			b.global_position = midle.global_position
 
 
 
@@ -53,6 +55,7 @@ func _process(delta: float) -> void:
 	start_rundy()
 
 func timeout() -> void:
+	Placeholder.get_child(0).queue_free()
 	generate()
 	timer.start()
 	
