f36d34be41109070e1d49dcaea48a46652cd1fc8
mxels <mikolajmeg07@gmail.com>
2025-05-12 00:00:37 +0200

    boss

---
+++ Enemy/bossowie/boss_3.gd
@@ -0,0 +1,4 @@
+extends Node2D
+@export var Player: CharacterBody2D
+@onready var bullet_spawn: Node2D = $Bullet_Spawn
+@onready var rotating_bullet_spawner: Node2D = $Bullet_Spawn/Rotating_Bullet_Spawner
